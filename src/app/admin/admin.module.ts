6156403e805d9568c9c77b3c757da910314ae3cf
Yaroslav <yaroslavsoldat2@gmail.com>
2020-08-13 23:00:40 +0300

    psch

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -21,7 +21,10 @@ import { EditPageComponent } from './edit-page/edit-page.component';
       {
         path: '', component: AdminLayoutComponent, children: [
           { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
-          { path: 'login', component: LoginPageComponent }
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent },
+          { path: 'create', component: CreatePageComponent },
+          { path: 'post/:id/edit', component: EditPageComponent },
         ]
       }
     ])
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
@@ -18,7 +18,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {
